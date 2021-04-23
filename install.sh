8676fe1b11ef1f02cac735ac648da4516a78ce66
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-23 22:20:23 +0530

    technical analysis installation added

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz \
+  && sudo tar -xzf ta-lib-0.4.0-src.tar.gz \
+  && sudo rm ta-lib-0.4.0-src.tar.gz \
+  && cd ta-lib/ \
+  && sudo ./configure --prefix=/usr \
+  && sudo make \
+  && sudo make install \
+  && cd ~ \
+  && sudo rm -rf ta-lib/ \
+  && pip install ta-lib
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import make_app
 
 if __name__ == '__main__':
     app = make_app()
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", debug=True)
